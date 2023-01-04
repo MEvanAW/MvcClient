8e20989fa4c2b6a95bce807bb0f7984200d12bb9
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-04 10:37:52 +0700

    [SCE-3] Add IOrderService implementation (OrderService).

---
+++ MvcClient/Application/OrderService.cs
@@ -0,0 +1,13 @@
+﻿using MvcClient.Dtos.Order;
+using MvcClient.Models.Order;
+
+namespace MvcClient.Application
+{
+    public class OrderService : IOrderService
+    {
+        public IEnumerable<OrderModel> Filter(OrderFilterDto orderFilterDto)
+        {
+            return new List<OrderModel>();
+        }
+    }
+}
