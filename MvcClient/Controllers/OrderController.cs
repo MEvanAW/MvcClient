1ce62fcda6c6ea8dedc71d453eb1911ac804044d
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-04 10:11:15 +0700

    [SCE-3] Add OrderController + its Index action and view.

---
+++ MvcClient/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcClient.Models.Order;
+
+namespace MvcClient.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new List<OrderModel>());
+        }
+    }
+}
