be4579202a391f78b26fe9b1c07ab544add31e01
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-04 14:33:46 +0700

    [SCE-3] Correct miscs from Order listing.

--- MvcClient/Models/Order/OrderModel.cs
+++ MvcClient/Models/Order/OrderModel.cs
@@ -5,7 +5,7 @@ namespace MvcClient.Models.Order
     public class OrderModel
     {
         public Guid Id { get; set; }
-        public string BuyerName { get; set; } = "Name not found";
+        public string BuyerName { get; set; } = string.Empty;
         public DateTime CreatedDateTime { get; set; }
         public decimal TotalPrice { get; set; }
         public OrderStatus Status { get; set; }
