bd343a2c00cda2cd16fd38a83dc3ae3db451e38c
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-05 10:23:16 +0700

    [SCE-3] Add CatalogController with its index action and view.

---
+++ MvcClient/Controllers/CatalogController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcClient.Models.Catalog;
+
+namespace MvcClient.Controllers
+{
+    public class CatalogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new List<CatalogListModel>());
+        }
+    }
+}
