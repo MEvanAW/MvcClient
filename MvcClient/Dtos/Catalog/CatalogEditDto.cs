85beb945cfdcf94dbaea8745a1b12813b1d7137c
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-05 16:37:36 +0700

    [SCE-3] Add CatalogEditDto.

---
+++ MvcClient/Dtos/Catalog/CatalogEditDto.cs
@@ -0,0 +1,28 @@
+﻿using MvcClient.Models.Catalog;
+
+namespace MvcClient.Dtos.Catalog
+{
+    public class CatalogEditDto : CatalogCreateDto
+    {
+        public Guid Id { get; set; }
+
+        public CatalogEditDto() { }
+
+        public CatalogEditDto(CatalogDetailsModel catalogDetailsModel)
+        {
+            Name = catalogDetailsModel.Name;
+            Description = catalogDetailsModel.Description;
+            Price = catalogDetailsModel.Price;
+            PictureFileName = catalogDetailsModel.PictureFileName;
+            PictureUri = catalogDetailsModel.PictureUri;
+            CatalogTypeId = catalogDetailsModel.CatalogTypeId;
+            CatalogBrandId = catalogDetailsModel.CatalogBrandId;
+            AvailableStock = catalogDetailsModel.AvailableStock;
+            RestockThreshold = catalogDetailsModel.RestockThreshold;
+            MaxStockThreshold = catalogDetailsModel.MaxStockThreshold;
+            OnReorder = catalogDetailsModel.OnReorder;
+            IsHidden = catalogDetailsModel.IsHidden;
+            Id = catalogDetailsModel.Id;
+        }
+    }
+}
