21201336cdf08c201293c7e8565ad33266542bc5
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-05 13:11:47 +0700

    [SCE-3] Add IBrandTypeService and its implementation (BrandTypeService)

---
+++ MvcClient/Application/IBrandTypeService.cs
@@ -0,0 +1,11 @@
+﻿using MvcClient.Models.Catalog.Brand;
+using MvcClient.Models.Catalog.Type;
+
+namespace MvcClient.Application
+{
+    public interface IBrandTypeService
+    {
+        Task<IEnumerable<BrandModel>> GetAllBrand();
+        Task<IEnumerable<TypeModel>> GetAllType();
+    }
+}
