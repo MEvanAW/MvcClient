44f18774a150a8361a7644f727eac6f152b4d038
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-05 14:17:10 +0700

    [SCE-3] Add server side Blazor.

--- MvcClient/Program.cs
+++ MvcClient/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddServerSideBlazor();
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultScheme = "Cookies";
@@ -54,5 +55,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapBlazorHub();
 
 app.Run();
