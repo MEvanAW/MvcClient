196117cba1b5ae1b15508bd6f906503ea6e4332f
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-06 11:14:04 +0700

    [SCE-3] Add BasketController, its index action and the view.

---
+++ MvcClient/Controllers/BasketController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcClient.Models.Basket;
+
+namespace MvcClient.Controllers
+{
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new List<BasketListModel>());
+        }
+    }
+}
