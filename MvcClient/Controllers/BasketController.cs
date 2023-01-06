df90e92215e47f9a6799b4a84124ea0f240f5aa4
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-06 11:30:24 +0700

    [SCE-3] Make basket listing page works.

--- MvcClient/Controllers/BasketController.cs
+++ MvcClient/Controllers/BasketController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MvcClient.Models.Basket;
+using MvcClient.Application;
+using MvcClient.Dtos.Basket;
 
 namespace MvcClient.Controllers
 {
     public class BasketController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
         {
-            return View(new List<BasketListModel>());
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _basketService.Filter(new BasketFilterDto()));
         }
     }
 }
