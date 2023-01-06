ebac948195ad7aa42d31ba6da0e3e5889c0e5499
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-06 11:13:24 +0700

    [SCE-3] Add BasketListModel.

---
+++ MvcClient/Models/Basket/BasketListModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcClient.Models.Basket
+{
+    public class BasketListModel
+    {
+        public Guid Id { get; set; }
+        public string Buyer { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
