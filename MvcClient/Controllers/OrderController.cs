3275876aba51a9a144402f5945aff0b8c2ae0f52
Muhammad Evan Anindya Wahyuaji <evan.wahyuaji@xtremax.com>
2023-01-09 13:13:07 +0700

    [SCE-3] Add logging to OrderController.

--- MvcClient/Controllers/OrderController.cs
+++ MvcClient/Controllers/OrderController.cs
@@ -1,22 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcClient.Application;
 using MvcClient.Dtos.Order;
+using MvcClient.Models.Order;
 
 namespace MvcClient.Controllers
 {
     public class OrderController : Controller
     {
         private readonly IOrderService _orderService;
+        private readonly ILogger<OrderController> _logger;
         private const string _isDelete = "IsDelete";
         private const string _name = "Name";
+        private const string _isError = "IsError";
 
-        public OrderController(IOrderService orderService)
+        public OrderController(IOrderService orderService, ILogger<OrderController> logger)
         {
             _orderService = orderService;
+            _logger = logger;
         }
 
         public async Task<IActionResult> Index()
         {
+            _logger.LogInformation("Order Index call.");
             if (TempData.ContainsKey(_isDelete))
             {
                 ViewData[_isDelete] = TempData[_isDelete];
@@ -25,11 +30,25 @@ namespace MvcClient.Controllers
             {
                 ViewData[_name] = TempData[_name];
             }
-            return View(await _orderService.Filter(new OrderFilterDto()));
+            IEnumerable<OrderModel> orderModels;
+            try
+            {
+                orderModels = await _orderService.Filter(new OrderFilterDto());
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError("Error getting orders with the following exception: " + ex.Message + Environment.NewLine +
+                    ex.InnerException + Environment.NewLine +
+                    ex.StackTrace, ex, this);
+                orderModels = new List<OrderModel>();
+                ViewData[_isError] = true;
+            }
+            return View(orderModels);
         }
 
         public async Task<IActionResult> Details([FromRoute] Guid id)
         {
+            _logger.LogInformation("Order Details {guid} call.", id);
             var orderDetailDto = new OrderDetailDto {
                 Id = id
             };
@@ -38,6 +57,7 @@ namespace MvcClient.Controllers
 
         public async Task<IActionResult> Delete()
         {
+            _logger.LogInformation("Order Delete call.");
             await _orderService.Delete();
             TempData[_isDelete] = true;
             return RedirectToAction("Index");
